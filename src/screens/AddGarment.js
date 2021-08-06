aab4bd6af05b3a0230336fb551fd13303869bd89
ddawidroch <david.roch.2001@gmail.com>
2021-08-06 14:01:33 +0200

    addGarment component started mocking up

--- src/screens/AddGarment.js
+++ src/screens/AddGarment.js
@@ -1,6 +1,19 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Text, TextInput, TouchableOpacity } from "react-native";
+import { Icon } from "react-native-elements/dist/icons/Icon";
 
 export default function AddGarment() {
-  return <View />;
+  return (
+    <View>
+      <TouchableOpacity>
+        <Icon name="camera" size={120} color="white" style={{ backgroundColor: "gray", width: 150, borderRadius: 20 }} />
+      </TouchableOpacity>
+      <Text>Tipo de prenda</Text>
+      <TextInput />
+      <Text>Marca</Text>
+      <TextInput />
+      <Text>Color</Text>
+      <TextInput />
+    </View>
+  );
 }
